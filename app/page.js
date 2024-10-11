d4c3b2892446c494656b0bfa8ccc8b702add04f4
Dharshan Bellan <dbellan1291@gmail.com>
2024-10-11 15:45:47 -0700

    basic nextjs application

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6cxl font-bold">findr.</h1>
+    </div>
+  );
+}
